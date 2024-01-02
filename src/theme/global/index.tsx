a5c78e792ef6fd2c442cc9e2d6408f44841ece6d
vt415204 <ramon.pinho@vtal.com>
2024-01-01 21:23:15 -0300

    feat: update global styles; add css root variables

--- src/theme/global/index.tsx
+++ src/theme/global/index.tsx
@@ -3,10 +3,37 @@ import { createGlobalStyle, css } from 'styled-components';
 
 const GlobalStyles = createGlobalStyle<GlobalStylesProps>((props) => {
     const { $isDarkTheme, theme } = props;
-    const { typography } = theme;
+    const { typography, colors } = theme;
     const { fontFamily, baseFontSize } = typography;
 
     return css`
+        :root {
+            --raposinha-brown: ${colors.brown};
+            --raposinha-earth: ${colors.earth};
+            --raposinha-sand: ${colors.sand};
+            --raposinha-offwhite: ${colors.offwhite};
+            --raposinha-black: ${colors.black};
+            --raposinha-white: ${colors.white};
+
+            --raposinha-brown-btn-bg-color: var(--raposinha-brown);
+            --raposinha-brown-btn-text-color: var(--raposinha-offwhite);
+
+            --raposinha-earth-btn-bg-color: var(--raposinha-earth);
+            --raposinha-earth-btn-text-color: var(--raposinha-offwhite);
+
+            --raposinha-sand-btn-bg-color: var(--raposinha-sand);
+            --raposinha-sand-btn-text-color: var(--raposinha-offwhite);
+
+            --raposinha-offwhite-btn-bg-color: var(--raposinha-offwhite);
+            --raposinha-offwhite-btn-text-color: var(--raposinha-brown);
+
+            --raposinha-black-btn-bg-color: var(--raposinha-black);
+            --raposinha-black-btn-text-color: var(--raposinha-white);
+
+            --raposinha-white-btn-bg-color: var(--raposinha-white);
+            --raposinha-white-btn-text-color: var(--raposinha-black);
+        }
+
         * {
             box-sizing: border-box;
             font-family: ${fontFamily};
