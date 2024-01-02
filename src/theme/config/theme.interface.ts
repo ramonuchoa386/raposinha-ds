fa4e93977f3ea99bbce49d5997b09c4280d0d548
vt415204 <ramon.pinho@vtal.com>
2024-01-01 21:22:47 -0300

    feat: update theme interface; edit colors field key type

--- src/theme/config/theme.interface.ts
+++ src/theme/config/theme.interface.ts
@@ -1,10 +1,12 @@
+import { ColorPalette } from './colorpalette';
+
 export interface CustomTheme {
     name: 'LIGHT' | 'DARK';
     typography: {
         fontFamily: string;
         baseFontSize: string;
     };
-    colors: { [key: string]: string };
+    colors: { [key in ColorPalette]: string };
     breakpoints: {
         mobile: string;
         tablet: string;
